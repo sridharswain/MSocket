49333c9861d89880155c72ed61d694d3a7bc0d2c
Sparsha Saha <saha.sparsha@gmail.com>
2017-10-17 13:12:06 +0530

    partition for quicksort added

--- Algorithm_sample_sort.java
+++ Algorithm_sample_sort.java
@@ -3,6 +3,30 @@ class Algorithm_sample_sort
 {
   public static void main(String args[])
   {
-    
+
   }
+
+  int partition(int arr[], int low, int high)
+{
+    int pivot = arr[high];
+    int i = (low-1);
+    for (int j=low; j<high; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+
+    return i+1;
+}
+
 }
