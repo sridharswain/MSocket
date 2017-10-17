597cd3900041d79b72bef94eaeec85c433131b77
Sparsha Saha <saha.sparsha@gmail.com>
2017-10-17 14:52:52 +0530

    Client Sample Sort added and changes made

--- Algorithm_sample_sort.java
+++ Algorithm_sample_sort.java
@@ -2,14 +2,13 @@ import java.util.*;
 class Algorithm_sample_sort
 {
   static int arr[];
-  static int[] drive_sample_sort(int[] a)
+  public static int[] drive_sample_sort(int[] a)
   {
     arr=a;
-
     sort(arr,0,arr.length-1);
     return arr;
   }
-  static int partition(int arr[], int low, int high)
+  private static int partition(int arr[], int low, int high)
 {
     int pivot = arr[high];
     int i = (low-1);
@@ -33,7 +32,7 @@ class Algorithm_sample_sort
 }
 
 
-static void sort(int arr[], int low, int high)
+private static void sort(int arr[], int low, int high)
     {
         if (low < high)
         {
